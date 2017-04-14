05cc56d811cd2178140562a2e503737ec24f9ec5
github username <github email address>
2017-04-13 23:32:25 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
-def oxford_comma(array)
+# def oxford_comma(array)
+#   if array.length > 1
+#     last_item = []
+#     last_item.push(array.pop())
+#     last_item.unshift("and").join(" ")
+#     array.push(last_item)
+#     array.join(", ")
+#   else
+#     array.to_s
+#   end
+# end
 
+def oxford_comma(array)
+  if array.length > 2
+    last_item = []
+    last_item.push("and #{array.pop()}")
+    array.concat(last_item).join(", ")
+  elsif array.length == 2
+    last_item = []
+    last_item.push("and #{array.pop()}")
+    array.concat(last_item).join(" ")
+  else
+    array[0]
+  end
 end
